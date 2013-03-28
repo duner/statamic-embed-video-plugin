a1406ef8ff299a06b384d18f34e28950ae0e3d2f
duner <asduner@gmail.com>
2013-03-28 01:26:59 -0300

    Update pi.video.php

--- pi.video.php
+++ pi.video.php
@@ -1,52 +1,42 @@
 <?php
-class Plugin_video extends Plugin {
+class Plugin_Video extends Plugin {
 
-  var $meta = array(
-	    'name'       => 'Video',
-	    'version'    => '0.0.1',
-	    'author'     => 'Alex Duner',
-	    'author_url' => 'http://alexduner.com'
-	  );
-	
-	
-	public function youtube() {
-		$url		= $this->fetchParam('url', null); // defaults to no
-		$width		= $this->fetchParam('width', 640, 'is_numeric'); // defaults to 640
-		$height		= $this->fetchParam('height', 390, 'is_numeric'); // defaults to 390
-		$site_root  = Statamic::get_site_root();
+	public function index() {
+		$src		= $this->fetch_param('src', null); // defaults to null
+		$videoid 	= $this->fetch_param('videoid', null); // defaults to null;
+		$width		= $this->fetch_param('width', 640, 'is_numeric');
+		$height		= $this->fetch_param('height', 390, 'is_numeric');
 
-		$videoid = $this->getYouTubeVideoID($url)
-
-		if ($url ) {
-			// https://developers.google.com/youtube/iframe_api_reference#Loading_a_Video_Player
-			$iframe = '<iframe class="youtube video" type="text/html" width="'.$width .'" height="'.$height.'" src="http://www.youtube.com/embed/'.$videoid.'?enablejsapi=1&origin='.$site_root.'"></iframe>';
-			return $iframe;
+		if ($src) {
+			//http://stackoverflow.com/questions/6556559/youtube-api-extract-video-id
+			$pattern =
+				'%^						# Match any youtube URL
+				(?:https?://)?			# Optional scheme. Either http or https
+				(?:www\.)?				# Optional www subdomain
+				(?:						# Group host alternatives
+					youtu\.be/			# Either youtu.be,
+					| youtube\.com		# or youtube.com
+						(?:           	# Group path alternatives
+							/embed/     # Either /embed/
+							| /v/		# or /v/
+							| .*v=		# or /watch\?v=
+						)				# End path alternatives.
+				)						# End host alternatives.
+			([\w-]{10,12})  # Allow 10-12 for 11 char youtube id.
+			($|&).*         # if additional parameters are also in query string after video id.
+			$%x';
+			$result = preg_match($pattern, $src, $matches);
+			if (false !== $result) {
+				$videoid = $matches[1];
+			}
 		}
-		return '';
-	}
-	     
-	function getYouTubeVideoID($youtbe_url) {
-		// http://stackoverflow.com/questions/6556559/youtube-api-extract-video-id
-		$pattern =
-			'%^								# Match any youtube URL
-			(?:https?://)?					# Optional scheme. Either http or https
-			(?:www\.)?						# Optional www subdomain
-			(?:								# Group host alternatives
-				youtu\.be/					# Either youtu.be,
-				| youtube\.com				# or youtube.com
-					(?:           			# Group path alternatives
-						/embed/     		# Either /embed/
-						| /v/				# or /v/
-						| .*v=				# or /watch\?v=
-					)						# End path alternatives.
-			)								# End host alternatives.
-		([\w-]{10,12})  					# Allow 10-12 for 11 char youtube id.
-		($|&).*         					# if additional parameters are also in query string after video id.
-		$%x';
-		$result = preg_match($pattern, $youtube_url, $matches);
-		if (false !== $result) {
-			return $matches[1];
+
+		
+		 if ($videoid) {
+			$html = '<iframe class="youtube video" type="text/html" width="'.$width.'" height="'.$height.'" src="http://www.youtube.com/embed/'.$videoid.'?feature=oembed&wmode=opaque&enablejsapi=1" frameborder="0" allowfullscreen></iframe>';
+			return $html;
+		} else {
+			return '<code>This video is not pointed at a valid YouTube URL.</code>';
 		}
-		return false;
 	}
-?>
+}
