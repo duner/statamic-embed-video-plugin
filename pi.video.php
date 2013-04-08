da1c2dbeeebc8a9734a99a2520d82d5553df2f8d
Alex Duner <asduner@gmail.com>
2013-04-08 16:01:42 -0400

    Added support for responsive design by implementing FitVids.js

--- pi.video.php
+++ pi.video.php
@@ -1,21 +1,40 @@
 <?php
 class Plugin_Video extends Plugin {
-	
+
 	var $meta = array(
 		'name'       => 'Embed Videos',
 		'version'    => '0.1',
 		'author'     => 'Alex Duner', 
 		'author_url' => 'htpp://alexduner.com'
 	);
-	
-	public function index() {
+
+	public function youtube() {
 		$src		= $this->fetchParam('src', false, false, false, false); // defaults to false
 		$width		= $this->fetchParam('width', 640, 'is_numeric');
 		$height		= $this->fetchParam('height', 390, 'is_numeric');
 		$videoid	= $this->fetchParam('id', false, false, false, false); // defaults to false
+		$responsive	= $this->fetchParam('responsive', true, null, true); // defaults to true
+
+
+		$enableJS = false; //defaults to false
+		$noBranding = false; //defaults to false
+		$dispRelVid = false; //defaults to false
+		$loopVideo = false; //defaults to false
+		$playAuto = false; //defaults to false
+		$dispInfo = true; //defaults to true
+		$dispControls = true; //defaults to true
+
+		if ($enableJS) { $enablejsapi = 1; } else { $enablejsapi = 0; }
+		if ($noBranding) { $modestbranding = 1; } else { $modestbranding = 0; }
+		if ($dispRelVid) { $rel = 1; } else { $rel = 0; }
+		if ($loopVideo) { $loop = 1; } else { $loop = 0; }
+		if ($playAuto) { $autoplay = 1; } else { $autoplay = 0; }
+		if ($dispInfo) { $showinfo = 1; } else { $showinfo = 0; }
+		if ($dispControls) { $controls = 1; } else { $controls = 0; }
 
 		if ($src && ! $videoid) {
 			//http://stackoverflow.com/questions/6556559/youtube-api-extract-video-id
+			//http://stackoverflow.com/questions/5830387/how-to-find-all-youtube-video-ids-in-a-string-using-a-regex/5831191#5831191
 			$pattern =
 				'%^						# Match any youtube URL
 				(?:https?://)?			# Optional scheme. Either http or https
@@ -41,10 +60,22 @@ class Plugin_Video extends Plugin {
 		}
 
 		if ($videoid) {
-			$html = '<iframe class="youtube video" type="text/html" width="'.$width.'" height="'.$height.'" src="https://www.youtube.com/embed/'.$videoid.'" frameborder="0" allowfullscreen></iframe>';
+			$html = '<iframe class="youtube video" type="text/html" width="'.$width.'" height="'.$height.'" src="https://www.youtube.com/embed/'.$videoid.'?autoplay='.$autoplay.'&controls='.$controls.'&enablejsapi='.$enablejsapi.'&loop='.$loop.'&modestbranding='.$modestbranding.'&rel='.$rel.'&showinfo='.$showinfo.'" frameborder="0" allowfullscreen></iframe>';
+			if ($responsive) {
+				//Implemented using FitVids.js
+				$html .= '
+				<script src="_add-ons/video/js/jquery.min.js"></script>
+				<script src="_add-ons/video/js/jquery.fitvids.js"></script>
+				<script>
+				$(document).ready(function(){
+					// Target your .container, .wrapper, .post, etc.
+					$("body").fitVids();
+				  });
+				</script>
+				';
+			}
 			return $html;
 		}
-
 		return '<code>This video is not pointed at a valid YouTube URL.</code>';
 	}
 
